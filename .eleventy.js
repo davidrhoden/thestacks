79ac6a12b2f98a671086650f01d7e34b1dd91877
David Rhoden <david@davidrhoden.com>
2021-08-19 14:19:13 -0500

    different SEO copy.

--- .eleventy.js
+++ .eleventy.js
@@ -14,7 +14,7 @@ const pluginSEO = require("eleventy-plugin-seo");
 
   eleventyConfig.addPlugin(pluginSEO, {
     title: "The Stacks :: New Orleans' $$$$ Million Dollar $$$$ Party Band",
-    description: "They don't call us the Louisiana homewreckers and hellraisers for nothing. This is the website for The Stacks from New Orleans, Louisiana. We don't care if other bands use the name, there are a lot of us, but we've been using it since 1999.",
+    description: "They don't call us the Louisiana homewreckers and hellraisers for nothing. The Stacks are New Orleans' Million-Dollar Party Band. This is their website.",
     url: "https://thestackswebsite.com",
     author: "David Rhoden/The Stacks",
     twitter: "davidrhoden",

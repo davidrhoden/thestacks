e65def2ad482a22e1a9e668062bd5249f9eb39e7
David Rhoden <david@davidrhoden.com>
2021-08-27 00:41:42 -0500

    add code for slideshows

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -7,3 +7,44 @@ if (window.netlifyIdentity) {
     }
   });
 }
+
+$(document).ready(function() {
+  
+  var currentImage = 0;
+  var images = $('#viewport img').get();
+  var totalImages = images.length;
+  var firstImage = $('#viewport img:first');
+  var altText = $(firstImage).attr("alt");
+  console.log(altText);
+  firstImage.addClass("fadedIn");
+  $('#caption').html(altText);
+
+  function increaseImage() {
+    ++currentImage;
+    if(currentImage > (totalImages - 1)) {
+      currentImage = 0;
+    }
+  }
+  function decreaseImage() {
+    --currentImage;
+    if(currentImage < 0) {
+      currentImage = (totalImages - 1);
+    }
+  }
+
+  $('#buttonPrevious').on('click', function(){
+    $(images[currentImage]).stop().removeClass('fadedIn');
+    decreaseImage();
+    $(images[currentImage]).stop().addClass('fadedIn');
+    altText = $(images[currentImage]).attr("alt");
+    $('#caption').html(altText);
+  }); 
+  $('#buttonNext').on('click', function(){
+    $(images[currentImage]).stop().removeClass('fadedIn');
+    increaseImage();
+    $(images[currentImage]).stop().addClass('fadedIn');
+    altText = $(images[currentImage]).attr("alt");
+    $('#caption').html(altText);
+  });
+
+});
